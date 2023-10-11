89d9e79b5ee2b58cadbe48a73d416ff2b2afd590
LucBrungard <luc.brungard@laposte.net>
2023-10-11 10:28:40 +0200

    reduce img size + placeholders

--- src/components/tabs/accueil.vue
+++ src/components/tabs/accueil.vue
@@ -1,15 +1,25 @@
 <template>
     <v-timeline align="start" justify="center">
       <v-timeline-item 
-        v-for="event in historique"
+        v-for="event in historique as any"
         :key="event.date"
         dot-color="secondary">
         <v-card variant="elevated" class="mr-5 ml-5" elevation="10">
           <v-img
             v-if="event.image !== ''"
             :src="event.image"
+            :lazy-src="event.placeholder"
             cover
-          ></v-img>
+          >
+            <template v-slot:placeholder>
+              <div class="d-flex align-center justify-center fill-height">
+                <v-progress-circular
+                  color="grey-lighten-4"
+                  indeterminate
+                ></v-progress-circular>
+              </div>
+            </template>
+          </v-img>
           <v-card-title>{{ event.titre }}</v-card-title>
           <v-card-subtitle>{{ event.date }}</v-card-subtitle>
           <v-card-text>{{ event.text }}</v-card-text>
@@ -21,8 +31,14 @@
 <script lang="ts">
   import historique from '@/assets/historique.json'
 
+  const imgFolder = `${import.meta.env.BASE_URL}/historique`;
+
   for (let hist of historique) {
-    hist.image = `${import.meta.env.BASE_URL}/historique/${hist.image}`
+    Object.defineProperty(hist, 'placeholder', {
+      value: `${imgFolder}/placeholder/${hist.image}`,
+      writable: false,
+    });
+    hist.image = `${imgFolder}/${hist.image}`
   }
 
   export default {
